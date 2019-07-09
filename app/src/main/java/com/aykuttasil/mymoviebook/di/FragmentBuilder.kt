47a15fdc9beeb84611e97ed41d32559d77f607fa
Aykut Asil <aykuttasil@gmail.com>
2019-07-09 15:44:28 +0300

    screens deps

--- app/src/main/java/com/aykuttasil/mymoviebook/di/FragmentBuilder.kt
+++ app/src/main/java/com/aykuttasil/mymoviebook/di/FragmentBuilder.kt
@@ -1,15 +1,25 @@
 package com.aykuttasil.mymoviebook.di
 
+import com.aykuttasil.mymoviebook.di.scopes.PerFragment
+import com.aykuttasil.mymoviebook.ui.fragment.nowplayingmovies.NowPlayingMoviesFragment
+import com.aykuttasil.mymoviebook.ui.fragment.popularmovies.PopularMoviesFragment
+import com.aykuttasil.mymoviebook.ui.fragment.upcomingmovies.UpcomingMoviesFragment
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class FragmentBuilder {
 
-/*
     @PerFragment
     @ContributesAndroidInjector
-    abstract fun bindMainFragment(): MainFragment
+    abstract fun bindPopularMoviesFragment(): PopularMoviesFragment
 
- */
+    @PerFragment
+    @ContributesAndroidInjector
+    abstract fun bindUpcomingMoviesFragment(): UpcomingMoviesFragment
+
+    @PerFragment
+    @ContributesAndroidInjector
+    abstract fun bindNowPlayingMoviesFragment(): NowPlayingMoviesFragment
 
 }
--- app/src/main/java/com/aykuttasil/mymoviebook/di/ViewModelBuilder.kt
+++ app/src/main/java/com/aykuttasil/mymoviebook/di/ViewModelBuilder.kt
@@ -3,6 +3,9 @@ package com.aykuttasil.mymoviebook.di
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.aykuttasil.mymoviebook.ui.activity.main.MainViewModel
+import com.aykuttasil.mymoviebook.ui.fragment.nowplayingmovies.NowPlayingMoviesViewModel
+import com.aykuttasil.mymoviebook.ui.fragment.popularmovies.PopularMoviesViewModel
+import com.aykuttasil.mymoviebook.ui.fragment.upcomingmovies.UpcomingMoviesViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -14,6 +17,21 @@ internal abstract class ViewModelBuilder {
     @Binds
     @ViewModelKey(MainViewModel::class)
     abstract fun provideMainVieWModel(viewModel: MainViewModel): ViewModel
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(PopularMoviesViewModel::class)
+    abstract fun providePopularMoviesViewModel(viewModel: PopularMoviesViewModel): ViewModel
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(UpcomingMoviesViewModel::class)
+    abstract fun provideUpcomingMoviesViewModel(viewModel: UpcomingMoviesViewModel): ViewModel
+
+    @IntoMap
+    @Binds
+    @ViewModelKey(NowPlayingMoviesViewModel::class)
+    abstract fun provideNowPlayingMoviesViewModel(viewModel: NowPlayingMoviesViewModel): ViewModel
 
     @Binds
     abstract fun provideViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
