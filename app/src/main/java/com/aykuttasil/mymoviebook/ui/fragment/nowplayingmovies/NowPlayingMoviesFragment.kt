b847febf69ac77a4bd6a70c36fae2b63c5f5f0bc
Aykut Asil <aykuttasil@gmail.com>
2019-07-09 15:43:12 +0300

    NowPlayingMoviesFragment added

---
+++ app/src/main/java/com/aykuttasil/mymoviebook/ui/fragment/nowplayingmovies/NowPlayingMoviesFragment.kt
@@ -0,0 +1,34 @@
+package com.aykuttasil.mymoviebook.ui.fragment.nowplayingmovies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.aykuttasil.mymoviebook.R
+import com.aykuttasil.mymoviebook.databinding.FragmentNowplayingmoviesBinding
+import com.aykuttasil.mymoviebook.di.ViewModelFactory
+import com.aykuttasil.mymoviebook.ui.fragment.BaseFragment
+import com.aykuttasil.mymoviebook.ui.fragment.popularmovies.PopularMoviesViewModel
+import javax.inject.Inject
+
+class NowPlayingMoviesFragment : BaseFragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    val viewModel by viewModels<PopularMoviesViewModel> { viewModelFactory }
+
+    lateinit var binding: FragmentNowplayingmoviesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_nowplayingmovies, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/aykuttasil/mymoviebook/ui/fragment/nowplayingmovies/NowPlayingMoviesViewModel.kt
@@ -0,0 +1,12 @@
+package com.aykuttasil.mymoviebook.ui.fragment.nowplayingmovies
+
+import android.app.Application
+import com.aykuttasil.mymoviebook.ui.BaseViewModel
+import javax.inject.Inject
+
+class NowPlayingMoviesViewModel @Inject constructor(
+    val app: Application
+) : BaseViewModel(app) {
+
+
+}
