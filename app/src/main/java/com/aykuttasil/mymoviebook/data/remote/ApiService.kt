f29d29925f330cba4ec4f18e1b6e7ecd6974e684
Aykut Asil <aykuttasil@gmail.com>
2019-07-10 18:45:07 +0300

    user entity deleted

--- app/src/main/java/com/aykuttasil/mymoviebook/data/remote/ApiService.kt
+++ app/src/main/java/com/aykuttasil/mymoviebook/data/remote/ApiService.kt
@@ -1,14 +1,10 @@
 package com.aykuttasil.mymoviebook.data.remote
 
-import com.aykuttasil.mymoviebook.data.entity.User
 import com.aykuttasil.mymoviebook.data.remote.model.MovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-
-    @GET("users")
-    suspend fun getUser(): List<User>
 
     @GET("movie/popular")
     suspend fun getPopularMovies(
